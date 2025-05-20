39cbab54b70fb8e2031ed6f9713fac5a5f3d6deb
PreQursor <preqursor.team@gmail.com>
2025-05-20 13:49:06 +0500

    Free match V2.0

--- src/screenshot/screenshot.module.ts
+++ src/screenshot/screenshot.module.ts
@@ -5,12 +5,14 @@ import { ScreenshotController } from './screenshot.controller';
 import { Screenshot, ScreenshotSchema } from 'src/screenshot/schema/screenshot.schema';
 import { AuthModule } from 'src/auth/auth.module';
 import { MailModule } from 'src/mail/mail.module';
+import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Screenshot.name, schema: ScreenshotSchema }]),
     AuthModule,
     MailModule,
+    WalletModule,
   ],
   controllers: [ScreenshotController],
   providers: [ScreenshotService],
